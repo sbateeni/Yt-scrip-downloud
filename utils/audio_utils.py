da1ab21967ef3542ed0f782a9564aed945246352
sbateeni <121227261+sbateeni@users.noreply.github.com>
2025-05-06 16:13:28 +0300

    .

--- utils/audio_utils.py
+++ utils/audio_utils.py
@@ -3,12 +3,15 @@ import yt_dlp
 import streamlit as st
 import ssl
 import certifi
+import urllib3
+import requests
 
 def download_audio(url, temp_dir):
     """تحميل الصوت من فيديو يوتيوب"""
     try:
         # تكوين SSL
         ssl_context = ssl.create_default_context(cafile=certifi.where())
+        urllib3.disable_warnings()
         
         audio_filename_template = os.path.join(temp_dir, 'audio')
         ydl_opts = {
@@ -30,32 +33,58 @@ def download_audio(url, temp_dir):
             },
             'http_headers': {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-            }
+            },
+            'source_address': '0.0.0.0',
+            'geo_bypass': True,
+            'geo_verification_proxy': None,
+            'extract_flat': False,
+            'ignoreerrors': True,
+            'no_color': True,
+            'prefer_insecure': True,
+            'http_chunk_size': 10485760,
         }
 
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            try:
+        # محاولة التحميل المباشر
+        try:
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
                 info_dict = ydl.extract_info(url, download=True)
-            except Exception as e:
-                st.warning("محاولة تحميل بديلة...")
-                # محاولة بديلة مع خيارات مختلفة
-                ydl_opts['nocheckcertificate'] = True
-                ydl_opts['extract_flat'] = True
+        except Exception as e:
+            st.warning("محاولة تحميل بديلة...")
+            # محاولة بديلة مع خيارات مختلفة
+            ydl_opts.update({
+                'nocheckcertificate': True,
+                'extract_flat': True,
+                'prefer_insecure': True,
+                'geo_bypass': True,
+                'geo_verification_proxy': None,
+                'source_address': '0.0.0.0',
+            })
+            
+            try:
                 with yt_dlp.YoutubeDL(ydl_opts) as ydl2:
                     info_dict = ydl2.extract_info(url, download=True)
+            except Exception as e2:
+                st.warning("محاولة تحميل نهائية...")
+                # محاولة نهائية مع خيارات أبسط
+                ydl_opts.update({
+                    'format': 'bestaudio',
+                    'extract_audio': True,
+                    'audio_format': 'wav',
+                    'nocheckcertificate': True,
+                    'prefer_insecure': True,
+                    'geo_bypass': True,
+                    'extract_flat': True,
+                })
+                with yt_dlp.YoutubeDL(ydl_opts) as ydl3:
+                    info_dict = ydl3.extract_info(url, download=True)
 
-            downloaded_filepath = None
-            
-            if 'requested_downloads' in info_dict and info_dict['requested_downloads']:
-                downloaded_filepath = info_dict['requested_downloads'][0]['filepath']
-            elif 'entries' in info_dict:
-                if info_dict['entries'] and 'requested_downloads' in info_dict['entries'][0] and info_dict['entries'][0]['requested_downloads']:
-                    downloaded_filepath = info_dict['entries'][0]['requested_downloads'][0]['filepath']
-            else:
-                for fname in os.listdir(temp_dir):
-                    if fname.startswith(os.path.basename(audio_filename_template)):
-                        downloaded_filepath = os.path.join(temp_dir, fname)
-                        break
+        downloaded_filepath = None
+        
+        # البحث عن الملف المحمل
+        for fname in os.listdir(temp_dir):
+            if fname.startswith(os.path.basename(audio_filename_template)):
+                downloaded_filepath = os.path.join(temp_dir, fname)
+                break
 
         if not downloaded_filepath:
             raise Exception("لم يتم العثور على الملف الصوتي بعد التحميل")
